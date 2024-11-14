187dadf935b2f504816982347feb6033d33e954d
aekanshkhandelwal <157788623+aekanshkhandelwal@users.noreply.github.com>
2024-11-14 11:47:58 +0530

    Add files via upload

--- social-media-website-main/index.js
+++ social-media-website-main/index.js
@@ -1,220 +1,165 @@
-// Sidebar
-const menuItems = document.querySelectorAll('.menu-item');
+import express from 'express';
+import { Server } from 'socket.io';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// Messages 
-const messageNotification = document.querySelector('#messages-notifications');
-const messages = document.querySelector('.messages');
-const message = messages.querySelectorAll('.message');
-const messageSearch = document.querySelector('#message-search');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// Theme
-const theme = document.querySelector('#theme');
-const themeModal = document.querySelector('.customize-theme');
-const fontSize = document.querySelectorAll('.choose-size span');
-var root = document.querySelector(':root');
-const colorPalette = document.querySelectorAll('.choose-color span');
-const Bg1 = document.querySelector('.bg-1');
-const Bg2 = document.querySelector('.bg-2');
-const Bg3 = document.querySelector('.bg-3');
+const PORT = process.env.PORT || 3500;
+const ADMIN = 'Admin';
 
-// ============== SIDEBAR ============== 
+const app = express();
 
-// Remove active class from all menu items
-const changeActiveItem = () => {
-    menuItems.forEach(item => {
-        item.classList.remove('active');
-    })
-}
+app.use(express.static(path.join(__dirname, 'public')));
 
-menuItems.forEach(item => {
-    item.addEventListener('click', () => {
-        changeActiveItem();
-        item.classList.add('active');
-        if(item.id != 'notifications') {
-            document.querySelector('.notifications-popup').style.display = 'none';
-        } else {
-            document.querySelector('.notifications-popup').style.display = 'block';
-            document.querySelector('#notifications .notification-count').style.display = 'none';
-        }
-    })
-})
-
-// ============== MESSAGES ============== 
-
-// Searches messages
-const searchMessage = () => {
-    const val = messageSearch.value.toLowerCase();
-    message.forEach(user => {
-        let name = user.querySelector('h5').textContent.toLowerCase();
-        if(name.indexOf(val) != -1) {
-            user.style.display = 'flex'; 
-        } else {
-            user.style.display = 'none';
-        }
-    })
-}
-
-// Search for messages
-messageSearch.addEventListener('keyup', searchMessage);
-
-// Highlight messages card when messages menu item is clicked
-messageNotification.addEventListener('click', () => {
-    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
-    messageNotification.querySelector('.notification-count').style.display = 'none';
-    setTimeout(() => {
-        messages.style.boxShadow = 'none';
-    }, 2000);
-})
-
-// ============== THEME / DISPLAY CUSTOMIZATION ============== 
-
-// Opens Modal
-const openThemeModal = () => {
-    themeModal.style.display = 'grid';
-}
-
-// Closes Modal
-const closeThemeModal = (e) => {
-    if(e.target.classList.contains('customize-theme')) {
-        themeModal.style.display = 'none';
-    }
-}
-
-themeModal.addEventListener('click', closeThemeModal);
-theme.addEventListener('click', openThemeModal);
-
-
-// ============== FONT SIZE ============== 
-
-// Remove active class from spans or font size selectors
-const removeSizeSelectors = () => {
-    fontSize.forEach(size => {
-        size.classList.remove('active');
-    })
-}
-
-fontSize.forEach(size => { 
-    size.addEventListener('click', () => {
-        removeSizeSelectors();
-        let fontSize;
-        size.classList.toggle('active');
-
-        if(size.classList.contains('font-size-1')) { 
-            fontSize = '10px';
-            root.style.setProperty('----sticky-top-left', '5.4rem');
-            root.style.setProperty('----sticky-top-right', '5.4rem');
-        } else if(size.classList.contains('font-size-2')) { 
-            fontSize = '13px';
-            root.style.setProperty('----sticky-top-left', '5.4rem');
-            root.style.setProperty('----sticky-top-right', '-7rem');
-        } else if(size.classList.contains('font-size-3')) {
-            fontSize = '16px';
-            root.style.setProperty('----sticky-top-left', '-2rem');
-            root.style.setProperty('----sticky-top-right', '-17rem');
-        } else if(size.classList.contains('font-size-4')) {
-            fontSize = '19px';
-            root.style.setProperty('----sticky-top-left', '-5rem');
-            root.style.setProperty('----sticky-top-right', '-25rem');
-        } else if(size.classList.contains('font-size-5')) {
-            fontSize = '22px';
-            root.style.setProperty('----sticky-top-left', '-12rem');
-            root.style.setProperty('----sticky-top-right', '-35rem');
-        }
-
-        // Change font size of the root html element
-        document.querySelector('html').style.fontSize = fontSize;
-    })
-})
-
-// Remove active class from colors
-const changeActiveColorClass = () => {
-    colorPalette.forEach(colorPicker => {
-        colorPicker.classList.remove('active');
-    })
-}
-
-// Change color primary
-colorPalette.forEach(color => {
-    color.addEventListener('click', () => {
-        let primary;
-        changeActiveColorClass(); 
-
-        if(color.classList.contains('color-1')) {
-            primaryHue = 252;
-        } else if(color.classList.contains('color-2')) {
-            primaryHue = 52;
-        } else if(color.classList.contains('color-3')) {
-            primaryHue = 352;
-        } else if(color.classList.contains('color-4')) {
-            primaryHue = 152;
-        } else if(color.classList.contains('color-5')) {
-            primaryHue = 202;
-        }
-
-        color.classList.add('active');
-        root.style.setProperty('--primary-color-hue', primaryHue);
-    })
-})
-
-// Theme Background Values
-let lightColorLightness;
-let whiteColorLightness;
-let darkColorLightness;
-
-// Changes background color
-const changeBG = () => {
-    root.style.setProperty('--light-color-lightness', lightColorLightness);
-    root.style.setProperty('--white-color-lightness', whiteColorLightness);
-    root.style.setProperty('--dark-color-lightness', darkColorLightness);
-}
-
-Bg1.addEventListener('click', () => {
-    // Add active class
-    Bg1.classList.add('active');
-    // Remove active class from the others
-    Bg2.classList.remove('active');
-    Bg3.classList.remove('active');
-    // Remove customized changes from local storage
-    window.location.reload();
+const expressServer = app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
 
-Bg2.addEventListener('click', () => {
-    darkColorLightness = '95%';
-    whiteColorLightness = '20%';
-    lightColorLightness = '15%';
-
-    // Add active class
-    Bg2.classList.add('active');
-    // Remove active class from the others
-    Bg1.classList.remove('active');
-    Bg3.classList.remove('active');
-    changeBG();
-});
-
-Bg3.addEventListener('click', () => {
-    darkColorLightness = '95%';
-    whiteColorLightness = '10%';
-    lightColorLightness = '0%';
-
-    // Add active class
-    Bg3.classList.add('active');
-    // Remove active class from the others
-    Bg1.classList.remove('active');
-    Bg2.classList.remove('active');
-    changeBG();
-});
-
-// ============== FRIEND REQUESTS ============== 
-// Accept Friend Request
-const acceptRequest = (button) => {
-    // Find the closest parent div with class "request" and hide it
-    const requestDiv = button.closest('.request');
-    requestDiv.style.display = 'none';
+// state
+const UsersState = {
+  users: [],
+  setUsers: function (newUsersArray) {
+    this.users = newUsersArray;
+  },
 };
 
-// Add event listener to all accept buttons
-document.querySelectorAll('.btn.btn-primary').forEach(button => {
-    button.addEventListener('click', () => {
-        acceptRequest(button);
-    });
+const io = new Server(expressServer, {
+  cors: {
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? false
+        : ['http://localhost:5500', 'http://127.0.0.1:5500'],
+  },
 });
+
+io.on('connection', (socket) => {
+  console.log(`User ${socket.id} connected`);
+
+  // Upon connection - only to user
+  socket.emit('message', buildMsg(ADMIN, 'Welcome to Chat App!'));
+
+  socket.on('enterRoom', ({ name, room }) => {
+    // leave previous room
+    const prevRoom = getUser(socket.id)?.room;
+
+    if (prevRoom) {
+      socket.leave(prevRoom);
+      io.to(prevRoom).emit(
+        'message',
+        buildMsg(ADMIN, `${name} has left the room`)
+      );
+    }
+
+    const user = activateUser(socket.id, name, room);
+
+    // Cannot update previous room users list until after the state update in activate user
+    if (prevRoom) {
+      io.to(prevRoom).emit('userList', {
+        users: getUsersInRoom(prevRoom),
+      });
+    }
+
+    // join room
+    socket.join(user.room);
+
+    // To user who joined
+    socket.emit(
+      'message',
+      buildMsg(ADMIN, `You have joined the ${user.room} chat room`)
+    );
+
+    // To everyone else
+    socket.broadcast
+      .to(user.room)
+      .emit('message', buildMsg(ADMIN, `${user.name} has joined the room`));
+
+    // Update user list for room
+    io.to(user.room).emit('userList', {
+      users: getUsersInRoom(user.room),
+    });
+
+    // Update rooms list for everyone
+    io.emit('roomList', {
+      rooms: getAllActiveRooms(),
+    });
+  });
+
+  // When user disconnects - to all others
+  socket.on('disconnect', () => {
+    const user = getUser(socket.id);
+    userLeavesApp(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        buildMsg(ADMIN, `${user.name} has left the room`)
+      );
+
+      io.to(user.room).emit('userList', {
+        users: getUsersInRoom(user.room),
+      });
+
+      io.emit('roomList', {
+        rooms: getAllActiveRooms(),
+      });
+    }
+
+    console.log(`User ${socket.id} disconnected`);
+  });
+
+  // Listening for a message event
+  socket.on('message', ({ name, text }) => {
+    const room = getUser(socket.id)?.room;
+    if (room) {
+      io.to(room).emit('message', buildMsg(name, text));
+    }
+  });
+
+  // Listen for activity
+  socket.on('activity', (name) => {
+    const room = getUser(socket.id)?.room;
+    if (room) {
+      socket.broadcast.to(room).emit('activity', name);
+    }
+  });
+});
+
+function buildMsg(name, text) {
+  return {
+    name,
+    text,
+    time: new Intl.DateTimeFormat('default', {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+    }).format(new Date()),
+  };
+}
+
+// User functions
+function activateUser(id, name, room) {
+  const user = { id, name, room };
+  UsersState.setUsers([
+    ...UsersState.users.filter((user) => user.id !== id),
+    user,
+  ]);
+  return user;
+}
+
+function userLeavesApp(id) {
+  UsersState.setUsers(UsersState.users.filter((user) => user.id !== id));
+}
+
+function getUser(id) {
+  return UsersState.users.find((user) => user.id === id);
+}
+
+function getUsersInRoom(room) {
+  return UsersState.users.filter((user) => user.room === room);
+}
+
+function getAllActiveRooms() {
+  return Array.from(new Set(UsersState.users.map((user) => user.room)));
+}
